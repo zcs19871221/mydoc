ab835b4779ca31ffa1ca3ca4306e8a1b81d0e76c
zcs <bjzhangchengsi@netease.internal>
2017-02-21 10:04:33 +0800

    aa

--- test/server.js
+++
@@ -1,7 +0,0 @@
-var http = require("http");
-var server = http.createServer( (req, res) => {
-	console.log(req.headers);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('okay');
-});
-server.listen(9981);
